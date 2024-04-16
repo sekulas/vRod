0ba6e3eb1bbe1705324d8aa7d8141cbed1afc78f
sekulas <sekula.sebastian22@gmail.com>
2024-04-16 16:55:59 +0200

    feat: Files names as consts

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -1,19 +1,21 @@
 mod setup;
+mod types;
+use crate::wal::WAL;
 
 use std::io;
 use std::path::PathBuf;
 
 pub struct Database {
     path: PathBuf,
+    wal: WAL,
     //TODO collections: todo!("Implement collections"),
     //TODO wal: Wal
 }
 
 impl Database {
-    pub fn new(path: PathBuf, name: String) -> Result<Self, io::Error> {
-        self::setup::create_database_directory(&path, &name)?;
-
-        Ok(Self { path })
+    pub fn new(path: PathBuf, name: String) -> Result<(), io::Error> {
+        self::setup::create_database(&path, &name)?;
+        Ok(())
     }
 
     pub fn load(path: PathBuf) -> Database {
---
+++ src/database/types.rs
@@ -0,0 +1,2 @@
+pub const WAL_FILE: &str = "vr_wal";
+pub const CONFIG_FILE: &str = "vr_config";
