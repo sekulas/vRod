2cc85866b7163720dfab57f8379728b59e7b3ea1
sekulas <sekula.sebastian22@gmail.com>
2024-05-16 22:52:07 +0200

    refactor: CreateCol & DropCol Commands moved to separate file

--- src/command_query_builder/commands/command_types.rs
+++ src/command_query_builder/commands/command_types.rs
@@ -1,92 +1,9 @@
 use super::Result;
 use crate::command_query_builder::{CQAction, Command};
-use crate::types::{ID_OFFSET_STORAGE_FILE, INDEX_FILE, STORAGE_FILE, WAL_FILE};
+use crate::types::WAL_FILE;
 use crate::wal::Wal;
 use std::fs;
 use std::path::{Path, PathBuf};
-
-pub struct CreateCollectionCommand {
-    path: PathBuf,
-    collection_name: String,
-}
-
-impl CreateCollectionCommand {
-    pub fn new(path: &Path, collection_name: String) -> Self {
-        CreateCollectionCommand {
-            path: path.to_owned(),
-            collection_name,
-        }
-    }
-}
-
-impl Command for CreateCollectionCommand {
-    fn execute(&self) -> Result<()> {
-        let collection_path = self.path.join(&self.collection_name);
-
-        fs::create_dir(&collection_path)?;
-        Wal::create(&collection_path.join(WAL_FILE))?;
-        fs::File::create(collection_path.join(STORAGE_FILE))?;
-        fs::File::create(collection_path.join(ID_OFFSET_STORAGE_FILE))?;
-        fs::File::create(collection_path.join(INDEX_FILE))?;
-
-        Ok(())
-    }
-
-    fn rollback(&self) -> Result<()> {
-        let collection_path = self.path.join(&self.collection_name);
-
-        if collection_path.exists() {
-            fs::remove_dir_all(collection_path)?;
-        }
-
-        Ok(())
-    }
-}
-
-impl CQAction for CreateCollectionCommand {
-    fn to_string(&self) -> String {
-        format!("CREATE {}", self.collection_name)
-    }
-}
-
-pub struct DropCollectionCommand {
-    pub path: PathBuf,
-    pub collection_name: String,
-}
-
-impl DropCollectionCommand {
-    pub fn new(path: &Path, collection_name: String) -> Self {
-        DropCollectionCommand {
-            path: path.to_owned(),
-            collection_name,
-        }
-    }
-}
-
-impl Command for DropCollectionCommand {
-    fn execute(&self) -> Result<()> {
-        let collection_path = self.path.join(&self.collection_name);
-
-        if !collection_path.exists() {
-            println!("Collection {:?} has not been found.", collection_path);
-            return Ok(());
-        }
-
-        fs::remove_dir_all(collection_path)?;
-
-        Ok(())
-    }
-
-    fn rollback(&self) -> Result<()> {
-        Ok(())
-    }
-}
-
-impl CQAction for DropCollectionCommand {
-    fn to_string(&self) -> String {
-        format!("DROP {}", self.collection_name)
-    }
-}
 
 pub struct TruncateWalCommand {
     pub target_path: PathBuf,
---
+++ src/command_query_builder/commands/create_collection.rs
@@ -0,0 +1,58 @@
+use super::Result;
+use crate::{
+    command_query_builder::{CQAction, Command},
+    database::DbConfig,
+    types::{DB_CONFIG, ID_OFFSET_STORAGE_FILE, INDEX_FILE, STORAGE_FILE, WAL_FILE},
+    wal::Wal,
+};
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
+
+pub struct CreateCollectionCommand {
+    path: PathBuf,
+    collection_name: String,
+}
+
+impl CreateCollectionCommand {
+    pub fn new(path: &Path, collection_name: String) -> Self {
+        CreateCollectionCommand {
+            path: path.to_owned(),
+            collection_name,
+        }
+    }
+}
+
+impl Command for CreateCollectionCommand {
+    fn execute(&self) -> Result<()> {
+        let collection_path = self.path.join(&self.collection_name);
+
+        fs::create_dir(&collection_path)?;
+        Wal::create(&collection_path)?;
+        fs::File::create(collection_path.join(STORAGE_FILE))?;
+        fs::File::create(collection_path.join(ID_OFFSET_STORAGE_FILE))?;
+        fs::File::create(collection_path.join(INDEX_FILE))?;
+
+        let mut db_config: DbConfig = DbConfig::load(&self.path.join(DB_CONFIG))?;
+        db_config.add_collection(&self.collection_name)?;
+
+        Ok(())
+    }
+
+    fn rollback(&self) -> Result<()> {
+        let collection_path = self.path.join(&self.collection_name);
+
+        if collection_path.exists() {
+            fs::remove_dir_all(collection_path)?;
+        }
+
+        Ok(())
+    }
+}
+
+impl CQAction for CreateCollectionCommand {
+    fn to_string(&self) -> String {
+        format!("CREATE {}", self.collection_name)
+    }
+}
