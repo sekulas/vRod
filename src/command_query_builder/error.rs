4b7643dd4cd7acb9733cc7bab851c238193eee89
sekulas <sekula.sebastian22@gmail.com>
2024-06-08 23:02:38 +0200

    feat: additional error types

--- src/command_query_builder/error.rs
+++ src/command_query_builder/error.rs
@@ -1,7 +1,16 @@
+use crate::collection::Error as CollectionError;
+use std::path::PathBuf;
+
 pub type Result<T> = core::result::Result<T, Error>;
 
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
+    #[error("Cannot determine the path to the collection.")]
+    CannotDetermineCollectionPath {
+        database_path: Option<PathBuf>,
+        collection_name: Option<String>,
+    },
+
     #[error("Collection '{collection_name}' already exists.")]
     CollectionAlreadyExists { collection_name: String },
 
@@ -13,6 +22,12 @@ pub enum Error {
 
     #[error("No name for the collection has been provided.")]
     MissingCollectionName,
+
+    #[error("Missing argument for the given command.")]
+    MissingArgument,
+
+    #[error("{description}")]
+    Collection { description: String },
 
     #[error(transparent)]
     Io(#[from] std::io::Error),
