b0b9f8a8759e3620ce5ff9a373b6d4ff9599b564
sekulas <sekula.sebastian22@gmail.com>
2024-10-07 15:14:05 +0200

    feat: rollback for reindex

--- src/cq/commands/reindex.rs
+++ src/cq/commands/reindex.rs
@@ -1,7 +1,10 @@
+use std::fs;
+
 use super::Result;
 use crate::{
     components::{collection::Collection, wal::Wal},
     cq::{CQAction, CQTarget, CQValidator, Command, Validator},
+    types::{INDEX_FILE, STORAGE_FILE},
 };
 
 pub struct ReindexCommand {
@@ -32,8 +35,23 @@ impl Command for ReindexCommand {
     fn rollback(&mut self, wal: &mut Wal) -> Result<()> {
         CQValidator::target_exists(&self.collection);
         wal.append(format!("ROLLBACK {}", self.to_string()))?;
+        //TODO: ### Is this rollback fine? Should it contain Rollback entry in old file?
+        //TODO: ### Is this okay that we are not doing this in collection abstraction as cannot load it?
 
-        println!("No ROLLBACK for REINDEX command provided. Commiting."); //TODO: Maybe rollback?
+        let path = self.collection.get_target_path();
+
+        let bak_strg_path = path.join(format!("{STORAGE_FILE}.bak"));
+        let bak_idx_path = path.join(format!("{INDEX_FILE}.bak"));
+
+        if (!bak_strg_path.exists()) || (!bak_idx_path.exists()) {
+            //TODO: ### Is this okay, readonly in this situation?
+            println!("No backup files found for ROLLBACK REINDEX.");
+        } else {
+            let cur_strg_path = path.join(STORAGE_FILE);
+            let cur_idx_path = path.join(INDEX_FILE);
+            fs::rename(bak_strg_path, cur_strg_path)?;
+            fs::rename(bak_idx_path, cur_idx_path)?;
+        }
 
         wal.commit()?;
         Ok(())
