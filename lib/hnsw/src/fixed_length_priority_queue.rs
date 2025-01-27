4812b6613bbeff1c2a219798ceaf99b95edf9805
sekulas <sekula.sebastian22@gmail.com>
2025-01-27 20:08:33 +0100

    chore: changed copyright date

--- lib/hnsw/src/fixed_length_priority_queue.rs
+++ lib/hnsw/src/fixed_length_priority_queue.rs
@@ -1,4 +1,4 @@
-// Copied from Project qdrant (https://github.com/qdrant/qdrant/tree/master)
+// Got from Project qdrant (https://github.com/qdrant/qdrant/tree/master)
 // Licensed under the Apache License, Version 2.0 (the "License");
 // See: http://www.apache.org/licenses/LICENSE-2.0
 
